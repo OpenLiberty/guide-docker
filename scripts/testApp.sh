f09e3567e20a07fee4a69ba7e2a4946f20825059
Priyanshi Patel <priyanshi.patel@ibm.com>
2021-05-18 14:48:12 -0400

    Update test scripts

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -13,7 +13,7 @@ docker build -t ol-runtime .
 docker run -d --name rest-app \
   -p 9080:9080 -p 9443:9443 \
   -v "$(pwd)"/target/liberty/wlp/usr/servers:/servers \
-  -u $(id -u) ol-runtime
+  -u "$(id -u)" ol-runtime
 
 sleep 60
 
